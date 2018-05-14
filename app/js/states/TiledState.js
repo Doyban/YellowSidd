5673edc02a148503c1b17d1f7f5296681dba8dd6
Daniel Danielecki <danieldanielecki@dmbp.home>
2018-05-14 09:44:23 +0200

    Fix: uncomment AdMob

--- app/js/states/TiledState.js
+++ app/js/states/TiledState.js
@@ -133,7 +133,7 @@ YellowSidd.TiledState.prototype.restart_level = function () {
     localStorage.removeItem('jump_plus_one_once'); // Remove jump_plus_one_once from localStorage.
     localStorage.removeItem('speed_plus_one_once'); // Remove speed_plus_one_once from localStorage.
 
-    // showInterstitialFunc(); // Display the AdMob interstitial ads.
+    showInterstitialFunc(); // Display the AdMob interstitial ads.
 
     this.game.state.restart(true, false, this.level_data); // Restart TiledState state.
   }
@@ -145,7 +145,7 @@ YellowSidd.TiledState.prototype.game_over = function () {
   localStorage.removeItem('jump_plus_one_once'); // Remove jump_plus_one_once from localStorage.
   localStorage.removeItem('speed_plus_one_once'); // Remove speed_plus_one_once from localStorage.
 
-  // showInterstitialFunc(); // Display the AdMob interstitial ads.
+  showInterstitialFunc(); // Display the AdMob interstitial ads.
 
   this.game.state.start('BootState', true, false, 'assets/levels/game_over.json', 'GameOverState'); // Start MenuState.
 
